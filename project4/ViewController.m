112af105a6db153c98d6581f31ab56747c3e0ad2
Wenzhi <jasonzhaowenzhi@gmail.com>
2016-05-13 10:52:56 -0500

    try by wenzhi 1st

--- project4/ViewController.m
+++ project4/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)viewWillAppear:(BOOL)animated{
+    NSLog(@"haha");
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
