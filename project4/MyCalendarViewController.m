ce88ea3437814469f7820d57846e5d3a15e2ba95
Yinchuan Zhou <yzhou088@gmail.com>
2016-05-16 17:06:01 -0500

    NSCalender

--- project4/MyCalendarViewController.m
+++ project4/MyCalendarViewController.m
@@ -7,54 +7,31 @@
 //
 #import "CustomCollectionViewCell.h"
 #import "MyCalendarViewController.h"
-@interface MyCalendarViewController ()
+#import "MonthCollectionHeaderView.h"
+@interface MyCalendarViewController (){
+}
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
 @implementation MyCalendarViewController
 #pragma Mark - view did load
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSDate *now = [NSDate date];
-    NSCalendar *calendar = [[NSCalendar alloc]
-                            initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
-    NSCalendarUnit units = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay;
-    NSDateComponents *components = [calendar components:units fromDate:now];
+}
+-(int)MonthDaysCaculator:(NSInteger)Section{
+    NSDate *today = [NSDate date]; //Get a date object for today's date
+    NSCalendar *c = [NSCalendar currentCalendar];
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    [components setMonth:(int)Section];
+    NSDate *MonthFromNow = [c dateByAddingComponents:components
+                                                 toDate:today
+                                                options:0];
     
-    NSLog(@"Day: %ld", (long)[components day]);
-    NSLog(@"Month: %ld", (long)[components month]);
-    NSLog(@"Year: %ld", (long)[components year]);
-    
-    
-//    NSCalendar *gregorian = [[NSCalendar alloc]
-//                             initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
-//    NSCalendar *buddhist = [[NSCalendar alloc]
-//                            initWithCalendarIdentifier:NSCalendarIdentifierChinese];
-//    NSCalendar *preferred = [NSCalendar currentCalendar];
-//    NSLog(@"%@", gregorian.calendarIdentifier);
-//    NSLog(@"%@", buddhist.calendarIdentifier);
-//    NSLog(@"%@", preferred.calendarIdentifier);
-    
-//    NSDate *now = [NSDate date];
-//    NSTimeInterval secondsInWeek = 7 * 24 * 60 * 60;
-//    NSDate *lastWeek = [NSDate dateWithTimeInterval:-secondsInWeek
-//                                          sinceDate:now];
-//    NSDate *nextWeek = [NSDate dateWithTimeInterval:secondsInWeek
-//                                          sinceDate:now];
-//    NSLog(@"Last Week: %@", lastWeek);
-//    NSLog(@"Right Now: %@", now);
-//    NSLog(@"Next Week: %@", nextWeek);
-    
-    
-//    NSDate *now = [NSDate date];
-//    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier: NSCalendarIdentifierGregorian];
-//    NSDateComponents *components = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:now];
-//    [components setHour:5];
-//    NSDate *today10am = [calendar dateFromComponents:components];
-//    NSLog(@"%@",today10am);
+    NSRange days = [c rangeOfUnit:NSCalendarUnitDay
+                           inUnit:NSCalendarUnitMonth
+                          forDate:MonthFromNow];
+    return (int)days.length;
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -66,20 +43,95 @@
     return 3;
 }
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-    return 31;
+//    return [self MonthDaysCaculator:section];
+    return 42;
+    
 }
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
     CustomCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
     
-    [cell.BtnOutlet setTitle:[NSString stringWithFormat:@"%d",indexPath.row+1] forState:UIControlStateNormal];
-    cell.lable.text = [NSString stringWithFormat:@"%d",indexPath.row+1];
+    
+        NSDate *now = [NSDate date];
+        NSCalendar * calendar = [NSCalendar currentCalendar];
+        
+        NSDateComponents *components = [[NSDateComponents alloc] init];
+        [components setMonth:(int)indexPath.section];
+        NSDate *MonthFromNow = [calendar dateByAddingComponents:components
+                                                  toDate:now
+                                                 options:0];
+    
+        NSDateComponents * currentDateComponents = [calendar components: NSCalendarUnitYear | NSCalendarUnitMonth fromDate: MonthFromNow];
+        
+        NSDate * startOfMonth = [calendar dateFromComponents: currentDateComponents];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"EEEE"];
+        NSString *prettyDate = [formatter stringFromDate:startOfMonth];
+        
+        int offset = 0;
+        if ([prettyDate isEqualToString:@"Monday"]) {
+            offset = 1;
+        }else if([prettyDate isEqualToString:@"Tuesday"]){
+            offset = 2;
+            
+        }else if([prettyDate isEqualToString:@"Wednesday"]){
+            offset = 3;
+            
+        }else if([prettyDate isEqualToString:@"Thursday"]){
+            offset = 4;
+            
+        }else if([prettyDate isEqualToString:@"Friday"]){
+            offset = 5;
+            
+        }else if([prettyDate isEqualToString:@"Saturday"]){
+            offset = 6;
+            
+        }else if([prettyDate isEqualToString:@"Sunday"]){
+            offset = 0;
+            
+        }
+        
+        if ([self MonthDaysCaculator:indexPath.section]+offset>indexPath.row) {
+            
+            if (indexPath.row >= offset) {
+                [cell.BtnOutlet setTitle:[NSString stringWithFormat:@"%d",indexPath.row+1-offset] forState:UIControlStateNormal];
+            }else{
+                [cell.BtnOutlet setTitle:@"" forState:UIControlStateNormal];
+            }
+        }else{
+            [cell.BtnOutlet setTitle:@"" forState:UIControlStateNormal];
+        }
+    
     return cell;
 }
-
-- (void)deselectItemAtIndexPath:(NSIndexPath *)indexPath animated:(BOOL)animated{
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath{
     
-}
+    NSDate *now = [NSDate date];
+    NSCalendar * calendar = [NSCalendar currentCalendar];
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    [components setMonth:(int)indexPath.section];
+    NSDate *MonthFromNow = [calendar dateByAddingComponents:components
+                                                     toDate:now
+                                                    options:0];
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    
+    NSString *pretty = [formatter stringFromDate:MonthFromNow];
+    NSString *prettyDate = [pretty substringToIndex:3];
 
+    
+    
+    
+    UICollectionReusableView *reusableview = nil;
+    if (kind == UICollectionElementKindSectionHeader) {
+        MonthCollectionHeaderView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"HeaderView" forIndexPath:indexPath];
+        NSString *title = prettyDate;
+        headerView.monthLabel.text = title;
+        reusableview = headerView;
+    }
+    return reusableview;
+}
 
 #pragma mark - Autolayout Setting
 - (CGSize)collectionView:(UICollectionView *)collectionView
@@ -93,7 +145,7 @@
                         layout:(UICollectionViewLayout *)collectionViewLayout
         insetForSectionAtIndex:(NSInteger)section{
     
-    UIEdgeInsets edge = UIEdgeInsetsMake(20, 0, 0, 0);//top, left, bottom, right;
+    UIEdgeInsets edge = UIEdgeInsetsMake(0, 0, 0, 0);//top, left, bottom, right;
     return edge;
 }
 - (CGFloat)collectionView:(UICollectionView *)collectionView
